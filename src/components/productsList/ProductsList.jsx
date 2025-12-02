acec5260526cbd204daa316010aa537ad694faed
hrach24 <hrachmuradyan2003@gmail.com>
2025-12-02 21:53:31 +0400

    add

--- src/components/productsList/ProductsList.jsx
+++ src/components/productsList/ProductsList.jsx
@@ -1,43 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductsList.scss";
 import logo from "@/assets/images/header/logo.webp";
+import Product from "@/components/productsList/product/Product.jsx";
+import { PRODUCTS } from "@/utils/products.js";
+
 const ProductsList = ({ data, selectedCategory }) => {
+  const [clickedProduct, setClickedProduct] = useState(null);
   return (
-    <ul className={"products__list"}>
-      {data.map((item) => (
-        <li
-          className={"products__list__item"}
-          data-aos={"fade-up"}
-          key={item.id}
-        >
-          <div className={"products__list__item__img"}>
-            <img src={item.image} alt={item.title} />
-            <div className={"list__category"}>{selectedCategory}</div>
-            <div className={"brand__logo"}>
-              <img src={logo} alt={"logo"} />
+    <>
+      <ul className={"products__list"}>
+        {data.map((item) => (
+          <li
+            className={"products__list__item"}
+            data-aos={"fade-up"}
+            key={item.id}
+            onClick={() => {
+              setClickedProduct(
+                PRODUCTS[selectedCategory].find((i) => i.id === item.id),
+              );
+            }}
+          >
+            <div className={"products__list__item__img"}>
+              <img src={item.image} alt={item.title} />
+              <div className={"list__category"}>{selectedCategory}</div>
+              <div className={"brand__logo"}>
+                <img src={logo} alt={"logo"} />
+              </div>
             </div>
-          </div>
-          <div className={"products__list__item__info"}>
-            <div className={"products__list__item__title medium"}>
-              {item.title}
+            <div className={"products__list__item__info"}>
+              <div className={"products__list__item__title medium"}>
+                {item.title}
+              </div>
+              <div className={"products__list__item_description small"}>
+                {item.description}
+              </div>
+              <div className={"products__list__item_certifications__wrapper"}>
+                {item.certifications.map((certification) => (
+                  <div
+                    className={"products__list__item_certification small"}
+                    key={certification.title}
+                  >
+                    {certification}
+                  </div>
+                ))}
+              </div>
             </div>
-            <div className={"products__list__item_description small"}>
-              {item.description}
-            </div>
-            <div className={"products__list__item_certifications__wrapper"}>
-              {item.certifications.map((certification) => (
-                <div
-                  className={"products__list__item_certification small"}
-                  key={certification.title}
-                >
-                  {certification}
-                </div>
-              ))}
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+      {clickedProduct !== null && (
+        <Product
+          clickedProduct={clickedProduct}
+          setClickedProduct={setClickedProduct}
+        />
+      )}
+    </>
   );
 };
 
---
+++ src/components/productsList/product/Product.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./Product.scss";
+import ShieldSvg from "@/assets/icons/shield.svg?react";
+import CloseIconSvg from "@/assets/icons/close-icon.svg?react";
+const Product = ({ clickedProduct, setClickedProduct }) => {
+  return (
+    <>
+      <div
+        className={"product__closer"}
+        onClick={() => setClickedProduct(null)}
+      ></div>
+      <div className={"product__wrapper"}>
+        <div className={"product"}>
+          <div className={"product__content"}>
+            <div className={"product__title"}>
+              <span>{clickedProduct.title}</span>
+              <div
+                className={"close__icon"}
+                onClick={() => setClickedProduct(null)}
+              >
+                <CloseIconSvg />
+              </div>
+            </div>
+            <div className={"product__imgWrapper"}>
+              <img src={clickedProduct.image} alt={clickedProduct.title} />
+            </div>
+            <div className={"product__description"}>
+              <div className={"description__title"}>Description</div>
+              <div className={"description"}>{clickedProduct.description}</div>
+            </div>
+            <div className={"product__specifications"}>
+              <div className={"product__specifications__title medium"}>
+                Key Specifications
+              </div>
+              <div className={"product__specifications__list"}>
+                {clickedProduct.specifications.map((item) => (
+                  <div className={"product__specifications__item"}>
+                    <div
+                      className={"product__specifications__item__title small"}
+                    >
+                      {item.title}
+                    </div>
+                    <div
+                      className={"product__specifications__item__description"}
+                    >
+                      {item.text}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className={"certifications"}>
+              <div className={"certifications__titleWrapper"}>
+                <div className={"certification__icon"}>
+                  <ShieldSvg />
+                </div>
+                <div className={"product__certification__title"}>
+                  Certifications
+                </div>
+              </div>
+              <div className={"product__certification__list"}>
+                {clickedProduct.certifications.map((item) => (
+                  <div className={"certification__certifications__item"}>
+                    {item}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
